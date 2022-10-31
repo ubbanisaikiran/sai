be98f1b62d74c1238cde3f134b52b7fb6b90a881
subbani <subbani@linkfields.com>
2022-10-31 16:29:54 +0530

    commit check

--- App/App.js
+++ App/App.js
@@ -1,11 +1,16 @@
-import { View, Text } from 'react-native'
+import { View, Text, Dimensions } from 'react-native'
 import React from 'react'
 
-
+const {width,height} = Dimensions.get("window")
 export default function App() {
   return (
     <View style={{backgroundColor:"green",flex:1}}>
-      <Text style={{backfaceVisibility:"white",width:"100%",height:"40"}}>App</Text>
+      <Text style={{
+      backgroundColor:"white",
+      width:"100%",
+      height:40,
+      fontSize:30,
+      }}>App</Text>
     </View>
   )
 }
